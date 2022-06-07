1fde9d840a0b9bef6818615bfcd8100ce23da286
funky-furry <94495915+funky-furry@users.noreply.github.com>
2022-06-07 19:28:31 -0300

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "IMPOSTER";
+const accessCode2 = "CACHORRO";
+const accessCode3 = "TOALHA";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("M P O T S E R I", 100,50)
+    fill("lightblue")
+    text("Dica: Algo muito suspeito", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("C H O R A R C O", 700,150)
+    fill("lightblue")
+    text("Dica: Algo que quando te vê começa a fazer barulho", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("T L A H O A", 100,250)
+    fill("lightblue")
+    text("Dica: Fica mais molhado quando mais agente seca", 100,270)
+
+}
